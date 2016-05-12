636c19cdc1742679fef9a5905c8f01eee2b00866
alex <johnnyalex@126.com>
2016-05-12 15:04:09 +0800

    alex0512

--- Apps/Home/Conf/config.php
+++ Apps/Home/Conf/config.php
@@ -3,15 +3,9 @@ return array(
 	//'配置项'=>'配置值'
 	//数据库配置信息
 	'DB_TYPE'   => 'mysql', // 数据库类型
-<<<<<<< HEAD
 	'DB_HOST'   => '127.0.0.1', // 服务器地址
 	'DB_NAME'   => 'project', // 数据库名
 	'DB_USER'   => 'root', // 用户名
-=======
-	'DB_HOST'   => '192.168.50.22', // 服务器地址
-	'DB_NAME'   => 'project', // 数据库名
-	'DB_USER'   => 'yy', // 用户名
->>>>>>> e4f34d0e1f20f1b37c059a3b940567e2687b44ec
 	'DB_PWD'    => '333', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => '', // 数据库表前缀 
@@ -19,6 +13,5 @@ return array(
 	'DEFAULT_MODULE' => 'Index',//默认模板
 	'URl_MODEL'=>'2',//url模板
 	'SESSION_AUTO_START'=>true,//是否开启session
-
 );
 ?>
