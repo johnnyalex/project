0f21854d10cf2851a2792c0d7c129410f7c12178
st <shentao119@163.com>
2016-05-18 09:49:19 +0800

    tt0518

--- Apps/Admin/Controller/ServiceController.class.php
+++ Apps/Admin/Controller/ServiceController.class.php
@@ -3,11 +3,17 @@ namespace Admin\Controller;
 use Think\Controller;
 class ServiceController extends Controller {
     public function index(){
-        $service=M('service');
-        $services=$service->select();
+        // $service=M('service');
+        $service=M();
+        // $sql = "SELECT A.*,B.id gid FROM service A,order B WHERE A.order_id=B.id";
+        $s=$servicc->table('service','order')->where('order.id=service.id')->select();
+        $a = $service->_sql();
+        var_dump($a);
+        // $services=$service->select();
         // var_dump($services);die;
-        
-        $this->assign('services',$services);
+        // var_dump($s);
+        // $s = $service->table()
+        $this->assign('s',$s);
 
         $this->display();
     }     
